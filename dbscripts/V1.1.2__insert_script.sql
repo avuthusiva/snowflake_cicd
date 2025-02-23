ad70dcfc805f48ce6092ee33880437716008fdf1
avuthusiva <asvr410@gmail.com>
2025-02-23 18:05:24 +0530

    insert data

---
+++ dbscripts/V1.1.2__insert_script.sql
@@ -0,0 +1,13 @@
+use warehouse my_warehouse;
+
+use role accountadmin;
+
+use schema my_db.new_schema;
+
+insert into dept values(50,'HR','Hyderabad');
+insert into dept values(60,'Admin','Bangalore');
+insert into dept values(70,'Finance','Chennai');
+insert into dept values(80,'IT','Pune');
+insert into dept values(90,'Sales','Mumbai');
+insert into dept values(100,'Marketing','Delhi');
+insert into dept values(110,'Operations','Kolkata');
