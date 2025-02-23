e8e8be9ffd3b894a104d0fba172385716a24e12a
avuthusiva <asvr410@gmail.com>
2025-02-23 17:37:23 +0530

    added changes into the file

--- dbscripts/test_data.sql
+++ dbscripts/test_data.sql
@@ -1,10 +1,12 @@
 use warehouse my_warehouse;
 
+use role accountadmin;
+
 use database my_db;
 
-create schema if not exists demo;
+create schema if not exists new_schema;
 
-use schema my_db.demo;
+use schema my_db.new_schema;
 
 create or replace table dept
 (
